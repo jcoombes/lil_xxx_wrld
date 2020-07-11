c95eb914e651078a3ff6dbd1d2ac8e9cc99275a3
Jamie <coombes.jd@gmail.com>
2020-07-11 20:27:27 +0100

    Make slider work

--- TextureProgress.gd
+++ TextureProgress.gd
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_VSlider_value_changed(value):
+	self.value = value
