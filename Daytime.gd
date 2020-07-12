52d3d8c438d99403bfd96a3e8c8f03632ee11a50
Blaine Rogers <blaine.w.rogers@gmail.com>
2020-07-12 14:49:32 +0100

    Mash space to skip cutscenes.

--- Daytime.gd
+++ Daytime.gd
@@ -5,7 +5,7 @@ signal fell_asleep
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var asleep_presses = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +19,11 @@ func _ready():
 
 func _on_Lyrics_faded_out():
 	emit_signal("fell_asleep")
+
+
+func _on_Fall_Asleep_button_up():
+	asleep_presses += 1
+	
+	# skip transition if the player clicks the button twice
+	if asleep_presses == 2:
+		emit_signal("fell_asleep")
