81650a19ed1af8e08279810ddd42ec437131d928
Jamie <coombes.jd@gmail.com>
2020-07-12 12:58:57 +0100

    styropixels

---
+++ Amethyst 2.gd
@@ -0,0 +1,25 @@
+extends TextureRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+var previously_made_visible = false
+
+func _on_VSlider_value_changed(value):
+	if value < 40:
+		self.visible = true
+		previously_made_visible = true
+	elif previously_made_visible and value >= 40:
+		self.visible = false
+		previously_made_visible = false
